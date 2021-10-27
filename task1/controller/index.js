2982f83c38d0de2c14c98b66219e848368367ba7
Ajitesh Singh <ajitesh@invideo.io>
2021-10-27 02:15:05 -0700

    handling errors

--- task1/controller/index.js
+++ task1/controller/index.js
@@ -9,55 +9,67 @@ SET_PREFERENCE = "setpreferences";
 
 const mainController = {
   landingPage(req, res) {
-    res.render(path.join(__dirname + "/../views/landing"));
+    try {
+      res.render(path.join(__dirname + "/../views/landing"));
+    } catch (err) {
+      res.status(500).send("Something went wrong! Please try again");
+    }
   },
   processUserInput(req, res) {
     /**
      * TODO refactor
      */
-    const action = req.body.action || req.query.action;
-    switch (action) {
-      case SURVEY:
-        let prevPage = req.session.lastpage;
-        processCurrentPage(req);
-        let lastpage = req.session.lastpage || 0;
-        checkIfSessionExists(req);
+    try {
+      const action = req.body.action || req.query.action;
+      switch (action) {
+        case SURVEY:
+          let prevPage = req.session.lastpage;
+          processCurrentPage(req);
+          let lastpage = req.session.lastpage || 0;
+          checkIfSessionExists(req);
 
-        if (req.body.answer) {
-          const id = questions.questions[prevPage].id;
-          req.session.answers[id] = req.body.answer;
-        }
-        if (lastpage >= questions.questions.length - 1) {
-          res.render(path.join(__dirname + "/../views/surveycomplete"));
-          return;
-        }
-        const question = questions.questions[lastpage];
-        const param = {
-          page: lastpage,
-          username: req.session.user,
-          preference: req.session.preference,
-          question: question,
-          answer: req.session.answers[question.id],
-          preference: req.session.preference || "horizontal",
-        };
-        res.render(path.join(__dirname + "/../views/survey"), param);
-        break;
-      case SET_PREFERENCE:
-        req.session.preference = req.body.preference;
-        res.writeHead(301, {
-          Location: req.headers.origin + "/controller?action=survey",
-        });
-        res.send();
-        break;
-      case MATCH:
-        const data = modelUtility.processMatches(req, res, (matches) => {
-          res.render(path.join(__dirname + "/../views/match"), matches);
-        });
-        break;
+          if (req.body.answer) {
+            const id = questions.questions[prevPage].id;
+            req.session.answers[id] = req.body.answer;
+          }
+          if (lastpage >= questions.questions.length - 1) {
+            res.render(path.join(__dirname + "/../views/surveycomplete"));
+            return;
+          }
+          const question = questions.questions[lastpage];
+          const param = {
+            page: lastpage,
+            username: req.session.user,
+            preference: req.session.preference,
+            question: question,
+            answer: req.session.answers[question.id],
+            preference: req.session.preference || "horizontal",
+          };
+          res.render(path.join(__dirname + "/../views/survey"), param);
+          break;
+        case SET_PREFERENCE:
+          req.session.preference = req.body.preference;
+          res.writeHead(301, {
+            Location: req.headers.origin + "/controller?action=survey",
+          });
+          res.send();
+          break;
+        case MATCH:
+          const data = modelUtility.processMatches(req, res, (matches) => {
+            res.render(path.join(__dirname + "/../views/match"), matches);
+          });
+          break;
+      }
+    } catch (err) {
+      res.status(500).send("Something went wrong! Please try again");
     }
   },
   getPreference(req, res) {
-    res.render(path.join(__dirname + "/../views/preferences"));
+    try {
+      res.render(path.join(__dirname + "/../views/preferences"));
+    } catch (err) {
+      res.status(500).send("Something went wrong! Please try again");
+    }
   },
 };
 
